b0bd3c41ad1acebf60d9f7426190330d8da0cddb
shaqdeff <shaquillendunda@gmail.com>
2022-07-07 14:40:27 +0300

    Create index.css file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './index.css';
 import Calculator from './components/Calculator';
 
 /* eslint-disable react/prefer-stateless-function */
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,7 +3,34 @@ import React from 'react';
 /* eslint-disable react/prefer-stateless-function */
 class Calculator extends React.Component {
   render() {
-    return null;
+    return (
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="operand">0</div>
+        </div>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button">÷</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button">+</button>
+        <button type="button" className="span-two">
+          0
+        </button>
+        <button type="button">.</button>
+        <button type="button">=</button>
+      </div>
+    );
   }
 }
 
