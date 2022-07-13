71b6cfbd719232dca2325f99efff9fd3bd949945
Zelalem1222 <zelalemfissha394@gmail.com>
2022-07-13 13:25:46 -0400

    Fix linter errors

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,28 +1,28 @@
-import calculate from "./__mocks__/calculate";
+import calculate from './__mocks__/calculate';
+
 describe('calculate', () => {
+  test('should return the correct result', () => {
+    const obj = { total: '0', next: '0', operation: 'x' };
+    expect(calculate(obj, 'AC')).toEqual({ total: null, next: null, operation: null });
+  });
 
-test("should return the correct result", () => {
-    const obj = {total: '0', next: '0', operation: 'x'};
-    expect(calculate(obj, 'AC')).toEqual({total: null, next: null, operation: null});
-})
-
-test("Number of button if there is an operation", () => {
+  test('Number of button if there is an operation', () => {
     const obj = { total: '2', next: '0', operation: 'x' };
     const btn = '2';
-    expect(calculate(obj, btn)).toStrictEqual({ total: '2', next: '2', operation: 'x' });  
-})
+    expect(calculate(obj, btn)).toStrictEqual({ total: '2', next: '2', operation: 'x' });
+  });
 
-let result = {total:'0' , next:'0' }
+  let result = { total: '0', next: '0' };
 
-test("5 - 3 should be 2", () => {
-result = calculate(result, '5');
-result = calculate(result, '-');
-result = calculate(result, '3');
-result = calculate(result, '=');
+  test('5 - 3 should be 2', () => {
+    result = calculate(result, '5');
+    result = calculate(result, '-');
+    result = calculate(result, '3');
+    result = calculate(result, '=');
     expect(result.total).toBe('2');
-})
+  });
 
-test('8 / 2 should be equale to 4', () => {
+  test('8 / 2 should be equale to 4', () => {
     result = calculate(result, '8');
     result = calculate(result, '÷');
     result = calculate(result, '2');
@@ -30,18 +30,11 @@ test('8 / 2 should be equale to 4', () => {
     expect(result.total).toBe('4');
   });
 
-
-test('8 % 2 should be equale to 0', () => {
+  test('8 % 2 should be equale to 0', () => {
     result = calculate(result, '8');
     result = calculate(result, '%');
     result = calculate(result, '2');
     result = calculate(result, '=');
     expect(result.total).toBe('0');
-  })
-})
-
-
-
-
-
-
+  });
+});
--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -1,27 +1,27 @@
-import operate from "./__mocks__/operate";
+import operate from './__mocks__/operate';
 
-describe("operate", () => {
+describe('operate', () => {
+  test('1 + 2 should return 3', () => {
+    expect(operate('1', '2', '+')).toBe('3');
+  });
 
-    test("1 + 2 should return 3", ()=> {
-        expect(operate("1", "2", "+")).toBe("3");
-    })
+  test('1 - 2 should return -1', () => {
+    expect(operate('1', '2', '-')).toBe('-1');
+  });
 
-    test("1 - 2 should return -1", ()=> {
-        expect(operate("1", "2", "-")).toBe("-1");
-    })
+  test('1 x 2 should return 2', () => {
+    expect(operate('1', '2', 'x')).toBe('2');
+  });
 
-    test("1 x 2 should return 2", ()=> {
-        expect(operate("1", "2", "x")).toBe("2");
-    })
+  test('1 divided by 2 should return 0.5', () => {
+    expect(operate('1', '2', '÷')).toBe('0.5');
+  });
+  test('4 mod 3 should return 1', () => {
+    expect(operate('4', '3', '%')).toBe('1');
+  });
 
-    test("1 divided by 2 should return 0.5", ()=> {
-        expect(operate("1", "2", "÷")).toBe("0.5");
-    })
-    test("4 mod 3 should return 1", ()=> {
-        expect(operate("4", "3", "%")).toBe("1");
-    })
-
-  it("should return the correct result", () => {
-    expect(operate("1", "0", "÷")).toBe("Can't divide by 0.");
-    expect(operate("1", "0", "%")).toBe("Can't find modulo as can't divide by 0.");
-  })});
+  it('should return the correct result', () => {
+    expect(operate('1', '0', '÷')).toBe("Can't divide by 0.");
+    expect(operate('1', '0', '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+});
